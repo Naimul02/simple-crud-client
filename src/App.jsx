bd78e91d3194410570819df99d2406c19917c09e
Naimul02 <naimulislum39@gmail.com>
2024-04-21 21:10:55 +0600

    commit no 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,21 @@ function App() {
     const user = { name, email };
     console.log(user);
 
-    fetch("http://localhost:5000/users" , {
-      method : 'POST',
-      headers : {
-        'content-type' : 'application/json'
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
       },
-      body : JSON.stringify(user)
+      body: JSON.stringify(user),
     })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("Users added successfully!");
+          form.reset();
+        }
+      });
   };
 
   return (
